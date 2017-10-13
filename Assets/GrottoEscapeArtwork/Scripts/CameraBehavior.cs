ec1b754ddf72f2d2366ab2b5e1281be7dffc286d
Pedro Zaroni <zaronipedro@outlook.com>
2017-10-13 17:58:18 -0300

    Add 1st level map

---
+++ Assets/GrottoEscapeArtwork/Scripts/CameraBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+	public Transform target; // what the camera is following
+	public float smoothing;
+
+	Vector3 offset;
+	float lowY;
+	float lowX;
+	float highY;
+	float highX;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+
+		highX = 786.82074f;
+		highY = 22.95f;
+		lowX = transform.position.x;
+		lowY = transform.position.y;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 targetCamPos = target.position + offset;
+
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing*Time.deltaTime);
+
+		if (transform.position.y < lowY)
+			transform.position = new Vector3 (transform.position.x, lowY, transform.position.z);
+		if (transform.position.y > highY)
+			transform.position = new Vector3 (transform.position.x, highY, transform.position.z);
+		if (transform.position.x < lowX)
+			transform.position = new Vector3 (lowX, transform.position.y, transform.position.z);
+		if (transform.position.x > highX)
+			transform.position = new Vector3 (highX, transform.position.y, transform.position.z);
+	}
+}
---
+++ Assets/GrottoEscapeArtwork/Scripts/PlayerBehavior.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+	// movement variables
+	public float maxSpeed;
+	private bool facingRight;
+
+	// jumping variables
+	bool grounded = false;
+	float groundCheckRadius = 0.2f;
+	public LayerMask groundLayer;
+	public Transform groundCheck;
+	public float jumpHeight;
+
+	private Animator animator;
+
+	private Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+		facingRight = true;
+	}
+
+	void Update() {
+		if (grounded && Input.GetAxis ("Jump") > 0) {
+			grounded = false;
+			animator.SetBool ("onGround", grounded);
+			rigidbody.AddForce (new Vector2 (0, jumpHeight));
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		// check if grounded
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+		animator.SetBool ("onGround", grounded);
+
+		float move = Input.GetAxis ("Horizontal");
+		animator.SetFloat ("speed", Mathf.Abs (move));
+		rigidbody.velocity = new Vector2 (move * maxSpeed, rigidbody.velocity.y);
+
+		if (move > 0 && !facingRight) {
+			flip ();
+		} else if (move < 0 && facingRight) {
+			flip ();
+		}
+	}
+
+	void flip() {
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+}
